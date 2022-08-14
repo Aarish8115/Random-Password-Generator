77d8a642dbc909fddd1d1aaef957151f6107c9a6
Aarish <mohdaarish127@gmail.com>
2022-08-14 06:57:39 +0530

    added comments

--- Random password.py
+++ Random password.py
@@ -1,13 +1,23 @@
-import random,pyperclip
+import random # importing rrandom module
+import pyperclip # importing pyperclip module
 
+# the password will be made of them choosen randomly
 Upper="QWERTYUIOPASDFGHJKLZXCVBNM"
 Lower="qwertyuiopasdfghjklmnbvcxz"
 Number="1234567890"
-
 Character="~`!@#$%\"^&*()_-+=[]:;\\'<>,./?"
 
+# so that the password contains everything - Uppercase,Lowercase,Numbers and characters
 all=Character+Upper+Lower+Number
-lenght=10
-password="".join(random.sample(all,lenght))
+
+# length of the password
+length=10
+
+# creating the password using random module
+password="".join(random.sample(all,length))
+
+# printing the password
 print("Your random generated password is\n"+password)
+
+# this line so that the password is automatically copied to your clipboard
 pyperclip.copy(password)
